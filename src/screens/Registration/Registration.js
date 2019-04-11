fda962deec9e90db208446dd922e2b2711919017
AlexeyKaryka <karikasv9805@gmail.com>
2019-04-11 12:10:43 +0300

    EPMCUXDA-6489 add fields validation at registration screen, little style refinements

--- src/screens/Registration/Registration.js
+++ src/screens/Registration/Registration.js
@@ -1,6 +1,14 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { Text, ScrollView, KeyboardAvoidingView } from "react-native";
+
+import {
+  validateEmail,
+  validatePassword,
+  validateFirstName,
+  validateLastName,
+  validatePhone
+} from "../../utils/validators";
 import { styles } from "./styles";
 import { Button, Input } from "../../components";
 
@@ -31,6 +39,11 @@ const Registration = props => {
     [fields.lastName]: lastNameDefault,
     [fields.phone]: phoneDefault
   });
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [firstNameError, setFirstNameError] = useState("");
+  const [lastNameError, setLastNameError] = useState("");
+  const [phoneError, setPhoneError] = useState("");
 
   const setRegistrationDataCommon = field => value =>
     setRegistrationData(prevData => ({ ...prevData, [field]: value }));
@@ -41,6 +54,19 @@ const Registration = props => {
   const onBackPress = () => {
     // TODO: figure out whether it's possible just to trigger default android back button
     props.onBackNavigation();
+  };
+  const onAuthFieldBlur = () => {
+    setEmailError(validateEmail(email));
+    setPasswordError(validatePassword(password));
+  };
+  const onFirstNameBlur = () => {
+    setFirstNameError(validateFirstName(firstName));
+  };
+  const onLastNameBlur = () => {
+    setLastNameError(validateLastName(lastName));
+  };
+  const onPhoneBlur = () => {
+    setPhoneError(validatePhone(phone));
   };
 
   return (
@@ -56,12 +82,17 @@ const Registration = props => {
           label="E-mail"
           textContentType="emailAddress"
           onChangeText={setRegistrationDataCommon(fields.email)}
+          error={emailError}
+          onTextInputBlur={onAuthFieldBlur}
         />
         <Input
           value={password}
           label="Пароль"
           textContentType="password"
           onChangeText={setRegistrationDataCommon(fields.password)}
+          error={passwordError}
+          onTextInputBlur={onAuthFieldBlur}
+          wrapperStyles={[styles.belowInput]}
         />
         <Text style={styles.hintText}>{hintText}</Text>
         <Input
@@ -69,18 +100,26 @@ const Registration = props => {
           label="Имя"
           textContentType="name"
           onChangeText={setRegistrationDataCommon(fields.firstName)}
+          error={firstNameError}
+          onTextInputBlur={onFirstNameBlur}
         />
         <Input
           value={lastName}
           label="Фамилия"
           textContentType="familyName"
           onChangeText={setRegistrationDataCommon(fields.lastName)}
+          error={lastNameError}
+          onTextInputBlur={onLastNameBlur}
+          wrapperStyles={[styles.belowInput]}
         />
         <Input
           value={phone}
           label="Телефон"
           textContentType="telephoneNumber"
           onChangeText={setRegistrationDataCommon(fields.phone)}
+          error={phoneError}
+          onTextInputBlur={onPhoneBlur}
+          wrapperStyles={[styles.belowInput, styles.lastInput]}
         />
         <Button
           caption="Регистрация"
