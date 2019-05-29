ab7fef8d9233b1fc60a163eff6b95987c9f70f22
Dzmitry Bartalevich <dimati.bart@gmail.com>
2019-05-29 09:37:05 +0300

    EPMCUXDA-6482 - fixed password validation and its message

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -12,5 +12,5 @@ export const makeRequiredValidator = errorMsg => value => {
 const emailRegExp = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 export const makeValidatorEmail = makeSimpleValidator(emailRegExp);
 /* eslint-disable-next-line */
-const passwordRegExp = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{10,30}$/;
+const passwordRegExp = /\w{4,}/;
 export const makeValidatorPassword = makeSimpleValidator(passwordRegExp);
