06d1222761da8269231d2eb351d50e5af6f2fe16
Maksim Kazlou <maksim.kazlou@epam.com>
2019-06-03 21:27:13 +0300

    EPMCUXDA-6642 - add menu for deleting

--- src/api/Observation.service.js
+++ src/api/Observation.service.js
@@ -7,7 +7,7 @@ import { BaseService } from "api";
 export default class ObservationService extends BaseService {
   async getObservations() {
     const token = await AsyncStorage.getItem("token");
-    
+
     return super
       .sendRequest(GET_OBSERVATIONS_ENDPOINT, "GET", token)
       .then(response => response.json());
--- src/screens/ObservationItem/ObservationItem.js
+++ src/screens/ObservationItem/ObservationItem.js
@@ -1,93 +1,108 @@
-import React from "react";
+import React, { PureComponent } from "react";
 import { View, Text, Image, ScrollView } from "react-native";
 import getInformationBlock from "./sections/InformationBlock";
-import Toolbar from "./sections/Toolbar";
+// import Toolbar from "./sections/Toolbar";
+import DeleteObservation from "../DeleteObservation";
+import Icon from "react-native-vector-icons/AntDesign";
 
 import { translate } from "../../i18n";
 import { styles } from "./styles";
 
 const img = require("../Observations/Observation/mockData/bird.jpg");
 
-const ObservationItem = (props) => {
-    const {
-        speciesMentioned: { species },
-        ring: { identificationNumber },
-        placeName,
-        date,
-        sexMentioned: { desc_rus: sex },
-        ageMentioned: { desc_rus: age },
-        status: { desc_rus: status },
-        remarks
-    } = props.navigation.state.params.ObservationItem;
+export default class ObservationItem extends PureComponent {
 
-    const onBackPress = () => {
-        props.navigation.goBack();
-    };
+    static navigationOptions = ({ navigation }) => ({
+        title: navigation.state.params.ObservationItem.speciesMentioned.species,
+        headerRight: (
+            <DeleteObservation />
+        ),
+        headerLeft: (
+            <Icon
+                name="arrowleft"
+                size={30}
+                color="white"
+                style={{ padding: 15 }}
+                onPress={() => navigation.goBack()}
+            />
+        )
+    })
 
-    return (
-        <View>
-            <Toolbar onBackPress={onBackPress} title={species} />
-            <ScrollView style={styles.ObservationItem}>
-                {/* <View style={styles.backImageWrap}>
+    render() {
+        const {
+            speciesMentioned: { species },
+            ring: { identificationNumber },
+            placeName,
+            date,
+            sexMentioned: { desc_rus: sex },
+            ageMentioned: { desc_rus: age },
+            status: { desc_rus: status },
+            remarks
+        } = this.props.navigation.state.params.ObservationItem;
+
+        return (
+            <View>
+                {/* <Toolbar onBackPress={onBackPress} title={species} /> */}
+                <ScrollView style={styles.ObservationItem}>
+                    {/* <View style={styles.backImageWrap}>
                 <Image style={styles.backImage} source={img} />
             </View> */}
-                <View style={styles.wrap}>
-                    <Text style={styles.species}>{species}</Text>
-                </View>
-                <View style={styles.images}>
-                    <Image style={styles.image} source={img} />
-                    <Image style={styles.image} source={img} />
-                </View>
-                <View style={styles.wrap}>
-                    <Text style={styles.header}>
-                        {translate("observationItem.rings")}
-                    </Text>
-                    <View style={styles.ring}>
-                        <Text style={styles.text}>
-                            {translate("observationItem.rightRing")}
+                    <View style={styles.wrap}>
+                        <Text style={styles.species}>{species}</Text>
+                    </View>
+                    <View style={styles.images}>
+                        <Image style={styles.image} source={img} />
+                        <Image style={styles.image} source={img} />
+                    </View>
+                    <View style={styles.wrap}>
+                        <Text style={styles.header}>
+                            {translate("observationItem.rings")}
                         </Text>
-                        <View style={styles.rightNumberWrap}>
-                            <Text style={styles.rightNumber}>{identificationNumber}</Text>
+                        <View style={styles.ring}>
+                            <Text style={styles.text}>
+                                {translate("observationItem.rightRing")}
+                            </Text>
+                            <View style={styles.rightNumberWrap}>
+                                <Text style={styles.rightNumber}>{identificationNumber}</Text>
+                            </View>
+                        </View>
+                        <View style={styles.ring}>
+                            <Text style={styles.text}>
+                                {translate("observationItem.leftRings")}
+                            </Text>
+                            <View style={styles.leftNumberWrap}>
+                                <Text style={styles.leftNumber}>{identificationNumber}</Text>
+                            </View>
                         </View>
                     </View>
-                    <View style={styles.ring}>
-                        <Text style={styles.text}>
-                            {translate("observationItem.leftRings")}
-                        </Text>
-                        <View style={styles.leftNumberWrap}>
-                            <Text style={styles.leftNumber}>{identificationNumber}</Text>
-                        </View>
-                    </View>
-                </View>
-                {getInformationBlock(
-                    translate("observationItem.dateHeader"),
-                    date
-                )}
-                {getInformationBlock(
-                    translate("observationItem.countryHeader"),
-                    placeName
-                )}
-                <View style={styles.line}></View>
-                {getInformationBlock(
-                    translate("observationItem.genderHeader"),
-                    sex
-                )}
-                {getInformationBlock(
-                    translate("observationItem.ageHeader"),
-                    age
-                )}
-                {getInformationBlock(
-                    translate("observationItem.lifeStatusHeader"),
-                    status
-                )}
-                <View style={styles.line}></View>
-                {getInformationBlock(
-                    translate("observationItem.commentHeader"),
-                    remarks
-                )}
-            </ScrollView>
-        </View>
-    );
-};
-
-export default ObservationItem;
+                    {getInformationBlock(
+                        translate("observationItem.dateHeader"),
+                        date
+                    )}
+                    {getInformationBlock(
+                        translate("observationItem.countryHeader"),
+                        placeName
+                    )}
+                    <View style={styles.line}></View>
+                    {getInformationBlock(
+                        translate("observationItem.genderHeader"),
+                        sex
+                    )}
+                    {getInformationBlock(
+                        translate("observationItem.ageHeader"),
+                        age
+                    )}
+                    {getInformationBlock(
+                        translate("observationItem.lifeStatusHeader"),
+                        status
+                    )}
+                    <View style={styles.line}></View>
+                    {getInformationBlock(
+                        translate("observationItem.commentHeader"),
+                        remarks
+                    )}
+                </ScrollView>
+            </View>
+        )
+    }
+}
