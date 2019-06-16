2a2d27a4b902d80373cf71c679ce76d729d0154f
Veranika Buzhynskaya <Veranika_Buzhynskaya@epam.com>
2019-06-16 21:39:05 +0300

    EPMCUXDA-6937 - add addObservation in Observation service

--- src/api/Observation.service.js
+++ src/api/Observation.service.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 import AsyncStorage from "@react-native-community/async-storage";
-import { GET_OBSERVATIONS_ENDPOINT } from "config";
+import { OBSERVATIONS_ENDPOINT } from "config";
 import { BaseService } from "api";
 /* eslint-enable */
 
@@ -9,7 +9,15 @@ export default class ObservationService extends BaseService {
     const token = await AsyncStorage.getItem("token");
 
     return super
-      .sendRequest(GET_OBSERVATIONS_ENDPOINT, "GET", token)
+      .sendRequest(OBSERVATIONS_ENDPOINT, "GET", token)
+      .then(response => response.json());
+  }
+
+  async addObservations(data) {
+    const token = await AsyncStorage.getItem("token");
+
+    return super
+      .sendRequest(OBSERVATIONS_ENDPOINT, "POST", token, data)
       .then(response => response.json());
   }
 }
