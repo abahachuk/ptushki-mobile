da4f24207c3499b857e96498f662db9fd1a6fcce
Veranika Buzhynskaya <Veranika_Buzhynskaya@epam.com>
2019-06-21 00:22:05 +0300

    EPMCUXDA-6937 - add dropdown data

--- src/screens/AddEditObservation/AddObservation.js
+++ src/screens/AddEditObservation/AddObservation.js
@@ -4,6 +4,10 @@ import ObservationBase from "./ObservationBase";
 import { translate } from "../../i18n";
 import InitialDataService from "../../api/InitialData.service";
 import extractDataByProperty from "./extractDataByProperty";
+import { birdSpecies } from "./mocks/birdSpecies";
+import { birdSex } from "./mocks/birdSex";
+import { birdCondition } from "./mocks/birdCondition";
+import { countries } from "./mocks/countries";
 
 class AddObservation extends PureComponent {
   constructor(props) {
@@ -15,12 +19,19 @@ class AddObservation extends PureComponent {
     const initialDataService = new InitialDataService();
     initialDataService.getData().then(data => {
       this.setState({
-        birdSexValues: extractDataByProperty(data.sex, "id"),
+        // birdSexValues: extractDataByProperty(data.sex, "id"),
+        // birdAgeValues: extractDataByProperty(data.age, "id"),
+        // birdSpeciesValues: extractDataByProperty(data.species, "species"),
+        // birdObstaclesValues: extractDataByProperty(data.condition, "desc_eng"),
+        // ringTypeValues: extractDataByProperty(data.ringingScheme, "id"),
+        // countryValues: extractDataByProperty(data.placeCode, "country")
+
+        birdSpeciesValues: extractDataByProperty(birdSpecies),
+        birdSexValues: extractDataByProperty(birdSex),
         birdAgeValues: extractDataByProperty(data.age, "id"),
-        birdSpeciesValues: extractDataByProperty(data.species, "species"),
-        birdObstaclesValues: extractDataByProperty(data.condition, "desc_eng"),
+        birdObstaclesValues: extractDataByProperty(birdCondition),
         ringTypeValues: extractDataByProperty(data.ringingScheme, "id"),
-        countryValues: extractDataByProperty(data.placeCode, "country")
+        countryValues: extractDataByProperty(countries)
       });
     });
   }
--- src/screens/AddEditObservation/extractDataByProperty.js
+++ src/screens/AddEditObservation/extractDataByProperty.js
@@ -1,9 +1,19 @@
 export default function extractDataByProperty(arr, property) {
+  if (property) {
+    return arr.map(item => {
+      return {
+        id: item.id,
+        label: item[property],
+        value: item[property]
+      };
+    });
+  }
+
   return arr.map(item => {
     return {
-      id: item.id,
-      label: item[property],
-      value: item[property]
+      id: item,
+      label: item,
+      value: item
     };
   });
 }
